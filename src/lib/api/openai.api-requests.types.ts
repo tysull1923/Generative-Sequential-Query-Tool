962ed0f3fd66369a6b66bb082fcd8d439e5792b9
Tyler Sullivan <tcsullivan19@gmail.com>
2025-01-06 18:45:00 -0500

    Fixing SequentialChat

--- src/lib/api/openai.api-requests.types.ts
+++ src/lib/api/openai.api-requests.types.ts
@@ -1,29 +1,28 @@
 export enum Role {
-    SYSTEM = 'system',
-    USER = 'user',
-    DEVELOPER = 'developer',
-    ASSISTANT = 'assistant'
-  }
-  
-  // API Request
-  
-  export interface ChatRequest {
-    id: string;
-    role: Role;
-    type: 'chat' | 'pause';
-    content?: string;
-    status: 'pending' | 'in-progress' | 'completed' | 'paused';
-    response?: ChatResponse;
-    isPaused?: boolean;
-    number: number;
-  }
-  
-  /**
-   * API Response interface
-   */
-  export interface ChatResponse  {
-    provider: Role;
-    content: string;
-    responseType: 'text' | 'code' | 'images';
-  }
-  
+	SYSTEM = 'system',
+	USER = 'user',
+	DEVELOPER = 'developer',
+	ASSISTANT = 'assistant'
+}
+
+// API Request
+
+export type ChatRequest = {
+	id: string;
+	role: Role;
+	type: 'chat' | 'pause';
+	content?: string;
+	status: 'pending' | 'in-progress' | 'completed' | 'paused';
+	response?: ChatResponse;
+	isPaused?: boolean;
+	number: number;
+}
+
+/**
+ * API Response interface
+ */
+export type ChatResponse = {
+	provider: Role;
+	content: string;
+	responseType: 'text' | 'code' | 'images';
+}
